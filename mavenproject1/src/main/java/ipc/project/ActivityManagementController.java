070e6df69844b84eb652a5d2f65ef23abf053eb8
theouto <tomas@otero.pro>
2026-05-15 11:41:43 +0200

    improved activity menu

--- mavenproject1/src/main/java/ipc/project/ActivityManagementController.java
+++ mavenproject1/src/main/java/ipc/project/ActivityManagementController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,24 +12,31 @@ import javafx.scene.control.Button;
 import javafx.stage.FileChooser;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.AnchorPane;
 
 import upv.ipc.sportlib.*;
 
-public class ActivityManagementController {
+public class ActivityManagementController implements Initializable {
     
     @FXML
-    private AnchorPane activityPane;
+    private VBox activityList;
     
     //Se asume que si el usuario está aquí, es porque está loggeado
-    public void initialize(URL url, ResourceBundle rb) 
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
     {
-            App.activities = App.sportApp.getUserActivities();
+        if (App.sportApp.getCurrentUser() != null)
+        {
+           App.activities = App.sportApp.getUserActivities();
             for (int i = 0; i < App.activities.size(); i++)
             {
                 Button activity = new Button(App.activities.get(i).getName());
-                activityPane.getChildren().add(activity);
-            }
+                final int index = i;
+                activity.setOnAction(e -> setAction(index));
+                activityList.getChildren().add(activity);
+            } 
+        }
     }
     
     @FXML
@@ -36,4 +44,10 @@ public class ActivityManagementController {
     
     @FXML
     private void back() throws IOException {App.setRoot("FXMLDocument");}
+    
+    private void setAction(int i)
+    {
+        App.mapPath = App.activities.get(i).getSuggestedMap().getImagePath();
+        try {back();} catch (IOException e) {System.out.println("Could not go back!");}
+    }
 }
