a1c4f1c209c4a1f89791a3959465a937fbf568a8
overhxz <hectorsist2@gmail.com>
2026-05-19 20:41:18 +0200

    settings almost done

--- mavenproject1/src/main/java/ipc/project/UserSettingsController.java
+++ mavenproject1/src/main/java/ipc/project/UserSettingsController.java
@@ -5,12 +5,68 @@ import javafx.fxml.FXML;
 
 import upv.ipc.sportlib.*;
 
+
 public class UserSettingsController {
 
+    @FXML
+    private TextField Correo2;
+
+    @FXML
+    private TextField Nombre2;
+
+    @FXML
+    private TextField Passworten2;
+
+    @FXML
+    private DatePicker Calendar2;
+
+    @FXML
+    private ImageField Foto2;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        Nombre2.setText(UserCreation.Nombre.getText());
+        Passworten2.setText(UserCreation.Passworten.getText());
+        Calendar2.setText(UserCreation.Calendar.getValue().toString());
+        Foto2.setImage(UserCreation.Foto.getImage());
+        
+        
+    }
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("FXMLDocument");
     }
+    
+     @FXML
+    private void editCorreo(ActionEvent event) throws IOException {
+        Correo2.setDisable(Correo2.isDisable());
+    }
+    
+     @FXML
+    private void editContraseña(ActionEvent event) throws IOException {
+        Passworten2.setDisable(Passworten2.isDisable());
+    }
+    
+     @FXML
+    private void editCalendar(ActionEvent event) throws IOException {
+        Calendar2.setDisable(Calendar2.isDisable());
+    }
+    
+     @FXML
+    private void editFoto(ActionEvent event) throws IOException {
+        UserCreation.fotoPerfil(event);
+        
+    }
+    
+    
+    
+     @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("FXMLDocument");
+    }
+    
+    
   
     private void login()
     {
@@ -27,4 +83,7 @@ public class UserSettingsController {
     {
         //App.sportApp.registerUser("nick", "email", "pass", LocalDate, "avatar/path");
     }
+
+
+
 }
